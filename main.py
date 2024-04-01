809375b7a7a2eace7d44d94dfa6e33f05b19a5ee
Ankit Babber <ankitbabber@gmail.com>
2024-04-01 12:56:00 -0500

    added the ability to process multiple books and generate reports of those books

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-import os
+# import os
+from pathlib import Path 
 
 def main(directory, books):
-  if os.path.isdir(directory) and len(books) > 0:
-    # get contents of books/ from file and add it to buffer
-    file_contents = get_contents_of_book(f"./{directory}/{books[0]}") 
-    # count the number of words in file
-    word_cnt = get_num_of_words(file_contents)
-    # create a char freq count
-    all_char_dict = char_freq_counter(file_contents)
-    # convert freq count dict into a sortable dict_list
-    char_list_dict = create_list_dict(all_char_dict)
-    gen_report(char_list_dict, word_cnt)
+  # if os.path.isdir(directory) and len(books) > 0:
+  if Path(directory).is_dir() and len(books) > 0:
+    for book in books:
+      if (Path.cwd() / directory / book).exists():
+        # get contents of books/ from file and add it to buffer
+        file_contents = get_contents_of_book(f"./{directory}/{book}") 
+        # count the number of words in file
+        word_cnt = get_num_of_words(file_contents)
+        # create a char freq count
+        all_char_dict = char_freq_counter(file_contents)
+        # convert freq count dict into a sortable dict_list
+        char_list_dict = create_list_dict(all_char_dict)
+        gen_report(char_list_dict, word_cnt, book)
   else: print("Please add books to the \'books/\' folder for analysis!")
 
 def get_contents_of_book(filepath):
@@ -42,12 +46,13 @@ def create_list_dict(dict):
 def sort_on(dict_list):
   return dict_list["num"]
 
-def gen_report(char_list_dict, word_count):
+def gen_report(char_list_dict, word_count, title):
   char_list_dict.sort(reverse=True, key=sort_on)
-  print("--- report of books/frankenstein.txt --- \n")
+  print(f"--- report of books/{title} --- \n")
   print(f"{word_count} words found in document. \n")
   for item in char_list_dict:
     if item["char"].isalpha():
       print(f"The \'{item["char"]}\' character was found {item["num"]} times")
+  print("\n")
 
-main("books", ["frankenstein.txt"])
+main("books", ["frankenstein.txt", "metamorphosis.txt"])
